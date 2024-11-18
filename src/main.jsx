7e6dd0bf56873a25c960e9b71f2cdd6d4c644e4b
freddyvelarde <freddvel.dev@gmail.com>
2024-11-18 08:42:43 -0400

    adding `styled-components` intead of css

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-// react router
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-//
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f8f9fa;
+    color: #333;
+    line-height: 1.6;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    font-weight: bold;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </StrictMode>,
 );
