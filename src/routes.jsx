f97f88ff03eb126f7feefda4d1bd8deb6de2da34
freddyvelarde <freddvel.dev@gmail.com>
2024-11-19 19:33:13 -0400

    Changes: Navbar just moved to `sections` directory.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MarcoTeoricoPage from "./pages/MarcoTeorico/MarcoTeorico";
-import DashboardLayout from "./components/DashboardLayout/Navbar";
+import DashboardLayout from "./sections/DashboardLayout/Navbar";
 
 const router = createBrowserRouter([
   {
